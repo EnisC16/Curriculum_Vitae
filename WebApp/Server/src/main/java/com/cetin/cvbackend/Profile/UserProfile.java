0cc0337ca1b1992a4720688545eb9391b97cce51
EnisC16 <enis.c16@outlook.com>
2020-11-28 00:37:35 +0100

    UserProfile Class created

---
+++ WebApp/Server/src/main/java/com/cetin/cvbackend/Profile/UserProfile.java
@@ -0,0 +1,61 @@
+package com.cetin.cvbackend.profile;
+
+import java.util.UUID;
+
+public class UserProfile {
+    private UUID userProfileId;
+    private String username;
+    private String userProfileImageLink; // S3 key
+
+
+    public UserProfile(UUID userProfileId,
+                        String username,
+                        String userProfileImageLink)
+    {
+        this.userProfileId = userProfileId;
+        this.username = username;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getUserProfileImageLink() {
+        return userProfileImageLink;
+    }
+
+    public String getUserProfileId() {
+        return userProfileId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof UserProfile))  return false;
+
+        UserProfile userProfile = (UserProfile) o;
+        return Objects.equals(userProfileId, userProfile.userProfileId) && 
+                Objects.equals(username, userProfile.username) && 
+                Objects.equals(userProfileImageLink, userProfile.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, userProfileImageLink);
+    }
+
+
+}
