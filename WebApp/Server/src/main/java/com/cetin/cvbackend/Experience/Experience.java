b188f4efb805eb74ca1a80bab4fa60005311e57a
EnisC16 <enis.c16@outlook.com>
2020-11-29 15:38:32 +0100

    added Experience Model

---
+++ WebApp/Server/src/main/java/com/cetin/cvbackend/Experience/Experience.java
@@ -0,0 +1,63 @@
+package com.cetin.cvbackend.profile;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+public class UserProfile {
+    private UUID userProfileId;
+    private String username;
+    private String userProfileImageLink; // S3 key
+
+
+    public UserProfile(UUID userProfileId,
+                        String username,
+                        String userProfileImageLink)
+    {
+        this.userProfileId = userProfileId;
+        this.username = username;
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Optional<String> getUserProfileImageLink() {
+        return Optional.ofNullable(userProfileImageLink);
+    }
+
+    public UUID getUserProfileId() {
+        return userProfileId;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserProfileImageLink(String userProfileImageLink) {
+        this.userProfileImageLink = userProfileImageLink;
+    }
+
+    public void setUserProfileId(UUID userProfileId) {
+        this.userProfileId = userProfileId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this) return true;
+        if (!(o instanceof UserProfile))  return false;
+
+        UserProfile userProfile = (UserProfile) o;
+        return Objects.equals(userProfileId, userProfile.userProfileId) &&
+                Objects.equals(username, userProfile.username) &&
+                Objects.equals(userProfileImageLink, userProfile.userProfileImageLink);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userProfileId, username, userProfileImageLink);
+    }
+
+
+}
---
+++ WebApp/Server/src/main/java/com/cetin/cvbackend/Experience/ExperienceService.java
@@ -0,0 +1,23 @@
+package com.cetin.cvbackend.Profile;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import com.cetin.cvbackend.profile.UserProfile;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserProfileService {
+
+    private final UserProfileDataAccessService userProfileDataAccessService;
+
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+        this.userProfileDataAccessService = userProfileDataAccessService;
+    }
+
+    List<UserProfile> getUserProfiles(){
+        return userProfileDataAccessService.getUserProfiles();
+    }
+
+}
